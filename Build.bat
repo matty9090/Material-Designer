406a7fd5bb4e27a98afba0306fe3762564cfc8fe
matty9090 <lowe.matthew@mail.com>
2019-08-17 00:39:16 +0100

    Fix batch file

--- Build.bat
+++ Build.bat
@@ -1,12 +1,10 @@
-@echo off
-
 SET generator="Visual Studio 15"
 SET config="Debug"
 SET test=""
 
-IF "%1" NEQ "" SET generator="%1"
-IF "%2" NEQ "" SET config="%2"
-IF "%3" NEQ "" SET test="%3"
+IF "%%1"=="" ( SET generator="%1" )
+IF "%%2"=="" ( SET config="%2" )
+IF "%%3"=="" ( SET test="%3" )
 
 IF NOT EXIST "Build/" mkdir Build
 
